2a22cb21717ce3b7d5764e7e611ce3e04ab131f7
duyuwei <yuwei.du@happyelements.com>
2024-10-22 11:43:46 +0800

    test

---
+++ Sources/SPMTest/Person.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  SPMTest
+//
+//  Created by duyuwei on 2024/10/22.
+//
+
+import Foundation
+
+class Person {
+    private var  age: Int = 0;
+    private var  name: String = "";
+}
